152b2f17dda2c42728b8d6ea9ae025d3a52450ca
sylenien <nikita@openreplay.com>
2022-09-02 17:52:50 +0200

    feat(tracker): error tags

--- tracker/tracker/src/main/index.ts
+++ tracker/tracker/src/main/index.ts
@@ -44,6 +44,11 @@ export type Options = Partial<
   __DISABLE_SECURE_MODE?: boolean
 }
 
+interface ErrorExtra {
+  tags: string[]
+  meta: Record<string, string | number>
+}
+
 const DOCS_SETUP = '/installation/setup-or'
 
 function processOptions(obj: any): obj is Options {
@@ -272,7 +277,7 @@ export default class API {
     }
   }
 
-  handleError = (e: Error | ErrorEvent | PromiseRejectionEvent) => {
+  handleError = (e: Error | ErrorEvent | PromiseRejectionEvent, extraInfo?: ErrorExtra) => {
     if (this.app === null) {
       return
     }
